713cb6171d289917aa4ae2fc6ed9997a6f5f5b78
cychann <skcbdlhc129@naver.com>
2021-05-16 11:08:45 +0900

    5/16

---
+++ inflearn-algorithm-class/python/3-9.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+a.insert(0, [0]*n)
+a.append([0]*n)
+for x in a:
+    x.insert(0, 0)
+    x.append(0)
+cnt = 0
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if(a[i-1][j] < a[i][j] and a[i+1][j] < a[i][j] and a[i][j-1] < a[i][j] and a[i][j+1] < a[i][j]):
+            cnt += 1
+print(cnt)
