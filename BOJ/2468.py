0facaf91113bf41287fa8119feb616b06717c452
cychann <skcbdlhc129@naver.com>
2021-11-02 21:55:44 +0900

    11/2

--- BOJ/2468.py
+++ BOJ/2468.py
@@ -1,10 +1,21 @@
 from collections import deque
 
+n = int(input())
+graph = []
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+max = 0
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    for j in range(n):
+        if graph[i][j] > max:
+            max = graph[i][j]
 
-def bfs(x, y, graph):
+
+def bfs(x, y, v, visited):
     queue = deque()
     queue.append((x, y))
-    graph[x][y] = 0
+    visited[x][y] = 1
 
     while queue:
         a, b = queue.popleft()
@@ -12,35 +23,23 @@ def bfs(x, y, graph):
             nx = a + dx[i]
             ny = b + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < n and graph[x][y] != 0:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] > v and visited[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    queue.append((nx, ny))
 
 
-n = int(input())
-graph = []
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-chk_cnt = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
-
-for r in range(2, n+1):
-    t_graph = [item[:] for item in graph]
+res = 0
+for r in range(max):
+    visited = [[0] * n for _ in range(n)]
     cnt = 0
 
     for i in range(n):
         for j in range(n):
-            if t_graph[i][j] <= r:
-                t_graph[i][j] = 0
-
-    for i in range(n):
-        for j in range(n):
-            if t_graph[i][j] != 0:
-                bfs(i, j, t_graph)
-
-                print(i, j, r)
+            if graph[i][j] > r and visited[i][j] == 0:
+                bfs(i, j, r, visited)
                 cnt += 1
-    chk_cnt.append(cnt)
+    if res < cnt:
+        res = cnt
 
-print(chk_cnt)
+print(res)
