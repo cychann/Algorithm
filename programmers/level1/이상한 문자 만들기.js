6a5dec6a661331e61303fd487eb7fb0879e43708
cychann <skcbdlhc129@naver.com>
2023-01-26 10:36:09 +0900

    1/26

---
+++ programmers/level1/이상한 문자 만들기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  return s
+    .split(" ")
+    .map((word) =>
+      word
+        .split("")
+        .map((w, idx) => (idx % 2 === 0 ? w.toUpperCase() : w.toLowerCase()))
+        .join("")
+    )
+    .join(" ");
+}
