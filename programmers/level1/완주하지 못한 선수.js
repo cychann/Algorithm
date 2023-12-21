6384be788c6e3522379e84686493e1bd0d7a90b9
cychann <skcbdlhc129@naver.com>
2023-12-21 22:21:09 +0900

    [해쉬]: 42576

---
+++ programmers/level1/완주하지 못한 선수.js
@@ -0,0 +1,37 @@
+// 내 풀이
+function solution(participant, completion) {
+  let result = "";
+
+  const sortedParticipant = participant.sort();
+  const sortedCompletion = completion.sort();
+
+  for (let i = 0; i < completion.length; i++) {
+    if (sortedParticipant[i] !== sortedCompletion[i]) {
+      result = sortedParticipant[i];
+      break;
+    }
+  }
+
+  return result === ""
+    ? sortedParticipant[sortedParticipant.length - 1]
+    : result;
+}
+
+// 다른 좋은 풀이 (해시를 이용)
+function solution(participant, completion) {
+  const map = new Map();
+
+  for (let i = 0; i < participant.length; i++) {
+    let a = participant[i],
+      b = completion[i];
+
+    map.set(a, (map.get(a) || 0) + 1);
+    map.set(b, (map.get(b) || 0) - 1);
+  }
+
+  for (let [k, v] of map) {
+    if (v > 0) return k;
+  }
+
+  return "nothing";
+}
