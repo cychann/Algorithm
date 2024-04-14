4cc0f24dc505a332ae02b8555bd7e0427775984f
cychann <skcbdlhc129@naver.com>
2024-04-14 21:11:06 +0900

    4/14

---
+++ programmers/level0/리스트 자르기.py
@@ -0,0 +1,14 @@
+def solution(n, slicer, num_list):
+    a,b,c = slicer
+    if n == 1:
+        num_list = num_list[:b+1]
+    elif n == 2:
+        num_list = num_list[a:]
+    elif n == 3:
+        num_list = num_list[a:b+1]
+    else:
+        num_list = num_list[a:b+1]
+        num_list = num_list[::c]
+        
+    return num_list
+        
