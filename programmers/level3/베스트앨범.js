1de9caf9e96c7e27fa67ed3357d3cfcb343cef94
cychann <skcbdlhc129@naver.com>
2024-05-29 22:59:26 +0900

    5/29
    Leve3: 베스트앨범(해시)

---
+++ programmers/level3/베스트앨범.js
@@ -0,0 +1,45 @@
+function solution(genres, plays) {
+  // 장르별 총 재생 횟수를 저장하는 Map
+  // 장르별 고유 번호의 재생 횟수를 저장하는 Map
+  const result = [];
+
+  const genreTotalMap = new Map();
+  const genreIdMap = new Map();
+
+  for (let i = 0; i < plays.length; i++) {
+    const genre = genres[i];
+    const play = plays[i];
+
+    genreTotalMap.set(genre, (genreTotalMap.get(genre) || 0) + play);
+    const genreData = genreIdMap.get(genre) || [];
+    genreData.push({ id: i, play });
+    genreIdMap.set(genre, genreData);
+  }
+
+  // 배열로 전환할때 내가 짯던 코드
+
+  const genreTotalMapToArr = [...genreTotalMap];
+  genreTotalMapToArr.sort((a, b) => b[1] - a[1]);
+
+  genreTotalMapToArr.forEach((genre) => {
+    const extractedGenreData = genreIdMap.get(genre[0]);
+
+    extractedGenreData.sort((a, b) => b.play - a.play);
+    extractedGenreData.slice(0, 2).forEach((data) => {
+      result.push(data.id);
+    });
+  });
+
+  return result;
+
+  // 더 간결한 코드
+  return [...genreTotalMap]
+    .sort((a, b) => b[1] - a[1])
+    .flatMap(([genre]) =>
+      genreIdMap
+        .get(genre)
+        .sort((a, b) => b.play - a.play || a.id - b.id)
+        .slice(0, 2)
+        .map((data) => data.id)
+    );
+}
