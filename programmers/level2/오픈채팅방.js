8a5d9fe392b6ae1d0426610c618c7d32138af0ac
cychann <skcbdlhc129@naver.com>
2024-08-06 21:40:02 +0900

    코테합 - 해시 (8/6)

--- programmers/level2/오픈채팅방.js
+++ programmers/level2/오픈채팅방.js
@@ -1,3 +1,4 @@
+// 풀이 1
 function solution(record) {
   const obj = {};
   let answer = [];
@@ -18,4 +19,34 @@ function solution(record) {
     }
     return result;
   }, []);
+}
+
+// 풀이 2 (2024.08.06)
+function solution(record) {
+  const obj = {};
+  const result = [];
+
+  for (const r of record) {
+    const [cmd, userId, userName] = r.split(" ");
+
+    if (cmd === "Enter" || cmd === "Leave") {
+      result.push(`${cmd} ${userId}`);
+    }
+
+    if (userName) {
+      obj[userId] = userName;
+    }
+  }
+
+  return result.map((r) => {
+    const [cmd, userId] = r.split(" ");
+
+    if (cmd === "Enter") {
+      return `${obj[userId]}님이 들어왔습니다.`;
+    }
+
+    if (cmd === "Leave") {
+      return `${obj[userId]}님이 나갔습니다.`;
+    }
+  });
 }
