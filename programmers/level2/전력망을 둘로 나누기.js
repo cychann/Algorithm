77eb8a3f43d3f57e81c1c6bfe31edc4c9572c111
cychann <skcbdlhc129@naver.com>
2024-12-15 23:40:49 +0900

    programmers lv2 - 전력망을 둘로 나누기 (완전 탐색)

---
+++ programmers/level2/전력망을 둘로 나누기.js
@@ -0,0 +1,34 @@
+function solution(n, wires) {
+  const graph = Array.from({ length: n + 1 }, () => []);
+  for (const [a, b] of wires) {
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+
+  function dfs(node, parent) {
+    let cnt = 1;
+    for (const child of graph[node]) {
+      if (child !== parent) {
+        cnt += dfs(child, node);
+      }
+    }
+
+    return cnt;
+  }
+
+  let minDiff = Infinity;
+  for (const [a, b] of wires) {
+    graph[a].splice(graph[a].indexOf[b], 1);
+    graph[b].splice(graph[a].indexOf[a], 1);
+
+    const cntA = dfs(a, b);
+    const cntB = n - cntA;
+
+    minDiff = Math.min(minDiff, Math.abs(cntA - cntB));
+
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+
+  return minDiff;
+}
