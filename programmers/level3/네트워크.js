ead4a77d24ae2f1d4e8f6931e9ddc2ee2685074b
cychann <skcbdlhc129@naver.com>
2025-06-21 20:28:16 +0900

    프로그래머스 lv3 - 네트워크

--- programmers/level3/네트워크.js
+++ programmers/level3/네트워크.js
@@ -20,3 +20,32 @@ function solution(n, computers) {
 
   return result;
 }
+
+// sol2
+/*
+1~n까지 탐색하면서, visted가 false인 경우에 +1
+이 과정에서 각 순회마다 dfs로 visited 중간 처리
+*/
+
+function solution(n, computers) {
+  let result = 0;
+
+  const visited = Array(n).fill(false);
+
+  function dfs(v) {
+    visited[v] = true;
+
+    for (let i = 0; i < computers[v].length; i++) {
+      if (computers[v][i] && !visited[i]) dfs(i);
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      result++;
+    }
+  }
+
+  return result;
+}
