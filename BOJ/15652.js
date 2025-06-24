39228179d92e7cc40cd03a1b076d3670548765f0
cychann <skcbdlhc129@naver.com>
2025-06-25 08:38:30 +0900

    [BOJ]: 15652 - N과 M(4) | 백트래킹 | S3

---
+++ BOJ/15652.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const [N, M] = input.split(" ").map(Number);
+
+const result = [];
+
+function backtrack(nums, start) {
+  if (nums.length === M) {
+    result.push(nums);
+    return;
+  }
+
+  for (let i = start; i <= N; i++) {
+    backtrack(nums.concat(i), i);
+  }
+}
+
+backtrack([], 1);
+
+result.forEach((arr) => {
+  console.log(arr.join(" "));
+});
